6e34c87647c5978f50203e21122d76d2e05424ba
Abdulkadir TÜRE <87595266+abdulkadrtr@users.noreply.github.com>
2023-07-25 18:27:34 +0300

    first commit

---
+++ app/src/main/java/com/example/sensorshare/MainActivity.kt
@@ -0,0 +1,290 @@
+package com.example.sensorshare
+import android.Manifest
+import android.annotation.SuppressLint
+import androidx.activity.result.contract.ActivityResultContracts
+import android.os.Bundle
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.StrictMode
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sensorshare.ui.theme.SensorShareTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import java.io.BufferedReader
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.OutputStream
+import java.io.PrintWriter
+import java.net.ServerSocket
+import java.net.Socket
+import java.util.concurrent.ExecutorService
+import android.util.Base64
+import java.util.concurrent.Executors
+import androidx.camera.core.ImageCapture.OutputFileOptions
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.VideoCapture
+
+
+
+
+var latitude: Double = 0.0
+var longitude: Double = 0.0
+var gyroscopeX: Float = 0.0f
+var gyroscopeY: Float = 0.0f
+var gyroscopeZ: Float = 0.0f
+var temperatureValue : Float = 0.0f
+var lightValue : Float = 0.0f
+var accelerometerX : Float = 0.0f
+var accelerometerY : Float = 0.0f
+var accelerometerZ : Float = 0.0f
+var orientationX : Float = 0.0f
+var orientationY : Float = 0.0f
+var orientationZ : Float = 0.0f
+var base64Image : String = ""
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SensorShareTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    CenteredButton()
+                }
+            }
+        }
+        //GPS Konum Bilgisi
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        val locationListener = MyLocationListener()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+        //JIROSOP Bilgisi
+        val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        val gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        val gyroscopeListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                gyroscopeX = event.values[0]
+                gyroscopeY = event.values[1]
+                gyroscopeZ = event.values[2]
+            }
+            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+        }
+        sensorManager.registerListener(gyroscopeListener, gyroscope, SensorManager.SENSOR_DELAY_NORMAL)
+        //Işık Bilgisi
+        val light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        val lightListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                lightValue = event.values[0]
+            }
+            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+        }
+        sensorManager.registerListener(lightListener, light, SensorManager.SENSOR_DELAY_NORMAL)
+
+        //Sıcaklık Bilgisi
+
+        val temperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+        val temperatureListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                temperatureValue = event.values[0]
+            }
+            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+        }
+        sensorManager.registerListener(temperatureListener, temperature, SensorManager.SENSOR_DELAY_NORMAL)
+        //İvmeölçer Bilgisi
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val accelerometerListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                accelerometerX = event.values[0]
+                accelerometerY = event.values[1]
+                accelerometerZ = event.values[2]
+            }
+            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+        }
+        sensorManager.registerListener(accelerometerListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        // Orientation Sensor
+        val orientation = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION)
+        val orientationListener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent) {
+                orientationX = event.values[0]
+                orientationY = event.values[1]
+                orientationZ = event.values[2]
+            }
+            override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+        }
+        sensorManager.registerListener(orientationListener, orientation, SensorManager.SENSOR_DELAY_NORMAL)
+        startServer()
+        println("Server Başlatıldı")
+    }
+    inner class MyLocationListener : LocationListener{
+        override fun onLocationChanged(location: Location){
+            latitude = location.latitude
+            longitude = location.longitude
+        }
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+}
+
+@Composable
+fun CenteredButton() {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { enableHotspot(context) },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Sistemi Başlat")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CenteredButtonPreview() {
+    SensorShareTheme {
+        CenteredButton()
+    }
+}
+fun enableHotspot(context: Context) {
+    val intent = Intent(Intent.ACTION_MAIN)
+    intent.setClassName("com.android.settings", "com.android.settings.TetherSettings")
+    context.startActivity(intent)
+}
+fun startServer() {
+    val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+    StrictMode.setThreadPolicy(policy)
+    val serverThread = Thread {
+        try {
+            val serverSocket = ServerSocket(8080)
+            while (true) {
+                val socket = serverSocket.accept()
+                val inputStream = BufferedReader(InputStreamReader(socket.getInputStream()))
+                val printWriter = PrintWriter(socket.getOutputStream(), true)
+                val request = inputStream.readLine()
+                println("SERVER INF $request")
+                if (request.startsWith("OPTIONS")) {
+                    printWriter.println("HTTP/1.1 200 OK")
+                    printWriter.println("Access-Control-Allow-Origin: *")
+                    printWriter.println("Access-Control-Allow-Methods: POST")
+                    printWriter.println("Access-Control-Allow-Headers: Content-Type")
+                    printWriter.flush()
+                } else if (request.startsWith("GET /gps HTTP/1.1")) {
+                    printWriter.println("HTTP/1.1 200 OK")
+                    printWriter.println("Access-Control-Allow-Origin: *")
+                    printWriter.println("Access-Control-Allow-Methods: POST")
+                    printWriter.println("Access-Control-Allow-Headers: Content-Type")
+                    printWriter.println("Content-Type: application/json")
+                    printWriter.println()
+                    printWriter.println("{\"latitude\": \" $latitude \", \"longitude\": \" $longitude \"}")
+                    printWriter.flush()
+                } else if (request.startsWith("GET /gyro HTTP/1.1")){
+                    printWriter.println("HTTP/1.1 200 OK")
+                    printWriter.println("Access-Control-Allow-Origin: *")
+                    printWriter.println("Access-Control-Allow-Methods: POST")
+                    printWriter.println("Access-Control-Allow-Headers: Content-Type")
+                    printWriter.println("Content-Type: application/json")
+                    printWriter.println()
+                    printWriter.println("{\"gyroscopeX\": \" $gyroscopeX \", \"gyroscopeY\": \" $gyroscopeY \", \"gyroscopeZ\": \" $gyroscopeZ \"}")
+                    printWriter.flush()
+                } else if (request.startsWith("GET /temperature HTTP/1.1")){
+                    printWriter.println("HTTP/1.1 200 OK")
+                    printWriter.println("Access-Control-Allow-Origin: *")
+                    printWriter.println("Access-Control-Allow-Methods: POST")
+                    printWriter.println("Access-Control-Allow-Headers: Content-Type")
+                    printWriter.println("Content-Type: application/json")
+                    printWriter.println()
+                    printWriter.println("{\"temperature\": \" $temperatureValue \"}")
+                    printWriter.flush()
+                } else if (request.startsWith("GET /light HTTP/1.1")){
+                    printWriter.println("HTTP/1.1 200 OK")
+                    printWriter.println("Access-Control-Allow-Origin: *")
+                    printWriter.println("Access-Control-Allow-Methods: POST")
+                    printWriter.println("Access-Control-Allow-Headers: Content-Type")
+                    printWriter.println("Content-Type: application/json")
+                    printWriter.println()
+                    printWriter.println("{\"light\": \" $lightValue \"}")
+                    printWriter.flush()
+                } else if(request.startsWith("GET /accelerometer HTTP/1.1")){
+                    printWriter.println("HTTP/1.1 200 OK")
+                    printWriter.println("Access-Control-Allow-Origin: *")
+                    printWriter.println("Access-Control-Allow-Methods: POST")
+                    printWriter.println("Access-Control-Allow-Headers: Content-Type")
+                    printWriter.println("Content-Type: application/json")
+                    printWriter.println()
+                    printWriter.println("{\"accelerometerX\": \" $accelerometerX \", \"accelerometerY\": \" $accelerometerY \", \"accelerometerZ\": \" $accelerometerZ \"}")
+                    printWriter.flush()
+                } else if(request.startsWith("GET /orientation HTTP/1.1")){
+                    printWriter.println("HTTP/1.1 200 OK")
+                    printWriter.println("Access-Control-Allow-Origin: *")
+                    printWriter.println("Access-Control-Allow-Methods: POST")
+                    printWriter.println("Access-Control-Allow-Headers: Content-Type")
+                    printWriter.println("Content-Type: application/json")
+                    printWriter.println()
+                    printWriter.println("{\"orientationX\": \" $orientationX \", \"orientationY\": \" $orientationY \", \"orientationZ\": \" $orientationZ \"}")
+                    printWriter.flush()
+                } else if(request.startsWith("GET /camera HTTP/1.1")){
+                    printWriter.println("HTTP/1.1 200 OK")
+                    printWriter.println("Access-Control-Allow-Origin: *")
+                    printWriter.println("Access-Control-Allow-Methods: POST")
+                    printWriter.println("Access-Control-Allow-Headers: Content-Type")
+                    printWriter.println("Content-Type: application/json")
+                    printWriter.println()
+                    printWriter.println("{\"base64Image\": \" $base64Image \"}")
+                    printWriter.flush()
+                }
+                socket.close()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+    serverThread.start()
+}
+
+
